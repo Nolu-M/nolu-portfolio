f3043c86c17f29f7bb4349b925e46009593afc6b
Nolubabalo Maxazana <nolubaalomaxazana@gmail.com>
2023-11-12 13:47:10 +0200

    Added more technologies icons

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,11 +8,19 @@ const About = () => {
                     <h3 className='mb-8'>History</h3>
                     <p></p>
                     <h3 className='mb-8'>Technologies</h3>
-                    <div className='flex'>
+                    <div className='flex grid grid-cols-4'>
                         <img className="w-24" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />
-                        <img className="w-24 ml-8" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flask/flask-original.svg" />
-                        <img className="w-24 ml-8" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
-
+                        <img className="w-24" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flask/flask-original.svg" />
+                        <img className="w-24" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
+                        <img className="w-24" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
+                        <img className="w-24" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg" />
+                        <img className="w-24" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/numpy/numpy-original.svg" />
+                        <img className="w-24" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
+                        <img className="w-24" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg" />
+                        <img className="w-24" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" />
+                        <img className="w-24" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" />
+                        <img className="w-24" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" />
+                        <img className="w-24" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
                     </div>
                     <h3 className='mt-8'>CV</h3>
                 </div>
