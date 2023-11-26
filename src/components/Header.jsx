1d7bd54897d7741ffd2ba0bd3ea76b36abae99c4
Nolubabalo Maxazana <nolubaalomaxazana@gmail.com>
2023-11-26 14:42:59 +0200

    added media query for header section.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,32 @@
-
+import { useState } from 'react';
 
 const Header = () => {
+    const [showMenu, setShowMenu] = useState(false);
+
+    const toggleLinks = () => {
+        setShowMenu(!showMenu);
+    };
+
+
+
     return (
         <section id='home'>
             <div className='container mx-auto'>
-                <div className='flex justify-between pt-4'>
-                    <a href="#home">
-                        <h1 className='text-[#6d4d6e]'>Nolubabalo Maxazana</h1>
+                <div className='flex justify-between pt-4 align-center'>
+                    <a href="#home"
+                        className='text-[#6d4d6e]'>Nolubabalo Maxazana
                     </a>
-                    <div className='text-[#6d4d6e]'>
+                    <button
+                        type='button'
+                        aria-controls="navbarNav"
+                        onClick={toggleLinks}
+                        className='menu-button bg-[#6d4d6e]'
+                    >
+                        <span className="material-symbols-outlined">
+                            menu
+                        </span>
+                    </button>
+                    <div className={`links text-[#6d4d6e] ${showMenu ? 'show-menu' : ''}`}>
                         <a href='#about' className='mr-4'>About</a>
                         <a href='#projects' className='mr-4'>Projects</a>
                         <a href='#links'>Links</a>
