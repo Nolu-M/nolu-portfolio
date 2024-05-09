aadb5e76e6978bdaba33127115362daa7b2815e0
Nolubabalo Maxazana <nolubabalomaxazana@gmail.com>
2024-05-09 14:34:33 +0200

    intro container

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -1,4 +1,5 @@
-import { AnimatePresence, motion } from "framer-motion"
+import { motion } from "framer-motion"
+import Hero from "../assets/images/hero.png"
 
 const Introduction = () => {
     return (
@@ -10,8 +11,28 @@ const Introduction = () => {
                 </div>
 
                 {/*content section*/}
+                <div className="w-full h-full flex flex-col items-center
+                lg:items-start justify-center gap-4">
+                    <h2 className="text-3xl lg:text-4xl text-white">Hello, It&apos;s me
+                    <span></span></h2>
+
+
+                </div>
+
+
+
+                {/*Hero image section*/}
                 <div className="w-full h-full flex items-center justify-center 
                 lg:items-center">
+                    <motion.img
+                    initial={{y : 0}}
+                    animate ={{ y : [-10, 10, -10]}}
+                    transition={{
+                        repeat: Infinity,
+                        duration: 2,
+                        ease: "linear",
+                    }}
+                    src={Hero} className="w-1/2 h-auto object-contain"/>
 
                 </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,15 +6,6 @@ export default {
   ],
   theme: {
     extend: {
-      fontFamily: {
-        'primary': ['Ubuntu', 'sans-serif'],
-        'secondary': ['Raleway', 'sans-serif'],
-        'tertiary': ['Dancing Script', 'cursive']
-      },
-      colors: {
-        'purple': '#7E22CE',
-        'gray': '#d1d5db'
-      },
       backgroundImage: {
         'background': "url('assets/images/bg.jpg')",
       },
